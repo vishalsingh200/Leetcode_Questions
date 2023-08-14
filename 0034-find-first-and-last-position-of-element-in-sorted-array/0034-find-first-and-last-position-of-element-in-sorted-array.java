0245d90f5654e49c3e71e6d67f262f65b669e465
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-08-15 00:42:15 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (60.05%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,17 +1,58 @@
-class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        //Brute force
-        int start = -1;
-        int end = -1;
+class Solution{
+    public int[] searchRange(int[] nums, int target){
+        int[] ans = {-1, -1};
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
         
-        for(int i = 0; i < nums.length; i++){
-            if(target == nums[i]){
-                if(start == -1){
-                    start = i;
+        ans[0] = start;
+        ans[1] = end;
+        
+        return ans;
+    }
+    static int search(int[] nums, int target, boolean indexFound){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(end >= start){
+            int mid = start + (end - start) / 2;
+            
+            if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else if(target > nums[mid]){
+                start = mid + 1;
+            }
+            else {
+                ans = mid;
+                if(indexFound){
+                    end = mid - 1;
                 }
-                end = i;
+                else{
+                    start = mid + 1;
+                }
             }
         }
-        return new int[]{start, end};
+        return ans;
     }
 }
+
+
+//Brute force
+
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+//         int start = -1;
+//         int end = -1;
+        
+//         for(int i = 0; i < nums.length; i++){
+//             if(target == nums[i]){
+//                 if(start == -1){
+//                     start = i;
+//                 }
+//                 end = i;
+//             }
+//         }
+//         return new int[]{start, end};
+//     }
+// }
