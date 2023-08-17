535d920f2cfadcdb29fbb73040aa7106dede7294
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-08-18 00:13:43 +0530

    Time: 1 ms (99.66%), Space: 57.7 MB (80.00%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,27 +1,26 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        if (nums.length == 0){
-            return 1;
-        }
-        for (int i = 0; i < nums.length;){
-            if (nums[i] > nums.length || nums[i] <= 0){
-                i++;
-            }
-            else if(nums[nums[i] - 1] == nums[i]){
-                i++;
+    public int firstMissingPositive(int[] arr) {
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] <= arr.length && arr[i] > 0 && arr[i] != arr[correct]){
+                swap(arr, i, correct);
             }
             else{
-                int temp = nums[nums[i] - 1];
-                nums[nums[i] - 1] = nums[i];
-                nums[i] = temp;
+                i++;
             }
         }
-        int i = 0;
-        for (i = 0; i < nums.length; i++){
-            if (nums[i] != i + 1){
-                return i + 1;
+        for(int index = 0; index < arr.length; index++){
+            if(arr[index] != index + 1){
+                return index + 1;
             }
         }
-        return nums[i - 1] + 1;
+        return arr.length + 1;
+    }
+    
+     static void swap(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
