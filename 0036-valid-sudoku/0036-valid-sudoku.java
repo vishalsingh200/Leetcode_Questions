2d1b2f7fd5ca56987d1229f9d63c9424a8f294ec
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-08-22 15:12:10 +0530

    Time: 23 ms (5.68%), Space: 44.3 MB (12.18%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,32 +1,16 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-    //      Set seen = new HashSet();
-    //      for (int i=0; i<9; ++i) {
-    //          for (int j=0; j<9; ++j) {
-    //              if (board[i][j] != '.') {
-    //                  String b = "(" + board[i][j] + ")";
-    //                  if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
-    //                      return false;
-    //         }
-    //     }
-    // }
-    // return true;
-       Set<String> seen = new HashSet<>();
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 9; j++){
-                char num = board[i][j];
-                if (num != '.')
-                    if (seen.contains(num + "_R_" + i) || 
-                        seen.contains(num + "_C_" + j) || 
-                        seen.contains(num + "_B_" + i/3 + "_" + j/3)){
-                        return false;
-                    }
-                else{
-                    seen.add(num + "_R_" + i);
-                    seen.add(num + "_C_" + j);
-                    seen.add(num + "_B_" + i/3 + "_" + j/3);
-                }
+        Set set = new HashSet<>();
+        for(int i = 0; i <9; i++){
+            for(int j = 0; j < 9; j++){
+               if (board[i][j] != '.') {
+                   String b = "(" + board[i][j] + ")";
+                   if (!set.add(b + i) || !set.add(j + b) || !set.add(i/3 + b + j/3))
+                       return false;
+                    
+               } 
             }
         }
-       return true;
-}}
+     return true;
+    }
+}
