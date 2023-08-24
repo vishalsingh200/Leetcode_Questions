95dceef2e67fea642424698229ca9b947e1e748b
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-08-24 19:55:19 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (50.78%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,14 +1,27 @@
 class Solution {
     public int numberOfSteps(int nums) {
-        return helper(nums, 0);
-    }
-    private static int helper(int nums, int steps){
-        if(nums == 0){
-            return steps;
+    //     return helper(nums, 0);
+    // }
+    // private static int helper(int nums, int steps){
+    //     if(nums == 0){
+    //         return steps;
+    //     }
+    //     if(nums % 2 == 0){
+    //         return helper(nums/2, steps+ 1);
+    //     }
+    //     return helper(nums - 1, steps + 1);
+    // 
+         int count = 0;
+        while (nums > 0){
+            if(nums % 2 == 0){
+                nums = nums / 2;
+                count++;
+            }
+            else {
+                nums = nums - 1;
+                count++;
+            }
         }
-        if(nums % 2 == 0){
-            return helper(nums/2, steps+ 1);
-        }
-        return helper(nums - 1, steps + 1);
+        return count;
     }
 }
