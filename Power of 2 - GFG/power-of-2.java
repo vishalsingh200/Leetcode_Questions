c880fd47239753710537098b59689efdc7626d90
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-09-14 02:15:59 +0530

    Added solution - LeetHub

--- Power of 2 - GFG/power-of-2.java
+++ Power of 2 - GFG/power-of-2.java
@@ -39,7 +39,14 @@ class Solution{
         if(n <= 0){
             return false;
         }
-        return (n & (n - 1)) == 0;
+        // return (n & (n - 1)) == 0;
+        while(n > 1){
+            if(n % 2 != 0){
+                return false;
+            }
+            n = n / 2;
+        }
+        return true;
     }
     
 }
