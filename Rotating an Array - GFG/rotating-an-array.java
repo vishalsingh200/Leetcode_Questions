93258129a261d50721249c9521cdddacc192ae49
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-09-24 03:26:34 +0530

    Added solution - LeetHub

--- Rotating an Array - GFG/rotating-an-array.java
+++ Rotating an Array - GFG/rotating-an-array.java
@@ -39,17 +39,30 @@ public class Main {
 
 class Solution {
     void leftRotate(int[] arr, int n, int d) {
-        d = d % n;
-        int[] temp = new int[d];
-        for(int i = 0; i < d; i++){
-            temp[i] = arr[i];
-        }
-        for(int i = d; i < n; i++){
-            arr[i - d] = arr[i]; 
-        }
+        // d = d % n;
+        // int[] temp = new int[d];
+        // for(int i = 0; i < d; i++){
+        //     temp[i] = arr[i];
+        // }
+        // for(int i = d; i < n; i++){
+        //     arr[i - d] = arr[i]; 
+        // }
         
-        for(int i = n - d; i < n; i++){
-            arr[i] = temp[i - (n - d)];
+        // for(int i = n - d; i < n; i++){
+        //     arr[i] = temp[i - (n - d)];
+        // }
+        d = d % n;
+        reverse(arr, 0 , d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+    void reverse(int[] arr, int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 }
