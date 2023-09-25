9778fa7bee52dca276105625ec5842d5b9720263
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-09-26 01:17:24 +0530

    Time: 35 ms (15.92%), Space: 56.9 MB (34.29%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,33 +1,33 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // Arrays.sort(nums);
-        // for(int i = 1; i < nums.length; i++){
-        //     if(nums[i] == nums[i - 1]){
-        //         return nums[i];
-        //     }
-        // }
-        // return -1;
-        int i = 0;
-        while(i < nums.length){
-            if(nums[i] != i + 1){
-                int correct = nums[i] - 1;
-                if(nums[i] != nums[correct]){
-                    swap(nums, i, correct);
-                }
-                else{
-                    return nums[i];
-                }
-            }
-            else{
-                i++;
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i - 1]){
+                return nums[i];
             }
         }
         return -1;
-    }
-    static void swap(int[] nums, int start, int end){
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
+    //     int i = 0;
+    //     while(i < nums.length){
+    //         if(nums[i] != i + 1){
+    //             int correct = nums[i] - 1;
+    //             if(nums[i] != nums[correct]){
+    //                 swap(nums, i, correct);
+    //             }
+    //             else{
+    //                 return nums[i];
+    //             }
+    //         }
+    //         else{
+    //             i++;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    // static void swap(int[] nums, int start, int end){
+    //     int temp = nums[start];
+    //     nums[start] = nums[end];
+    //     nums[end] = temp;
     }
     
 }
