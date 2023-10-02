323d30f0f1355431307d67e4d8b23f050fa4eae3
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-10-03 01:12:46 +0530

    Time: 2 ms (39.58%), Space: 43.9 MB (35.37%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,28 +1,71 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        helper(nums, 0, result);
-        return result;
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean fre[] = new boolean[nums.length];
+        find(nums, new Stack<>(), ans, fre);
+        return ans;
     }
-    static void helper(int[] nums, int index, List<List<Integer>> result){
-        if (index == nums.length - 1){
-            List<Integer> temp = new ArrayList<>();
-            for (int i = 0; i < nums.length; i++){
-                temp.add(nums[i]);
-            }
-            result.add(temp);
+    private void find(int[] arr, Stack<Integer> stack, List<List<Integer>> ans, boolean fre[]){
+        if(stack.size() == arr.length){
+            ans.add(new ArrayList<>(stack));
             return;
         }
-        for (int i = index; i < nums.length; i++){
-            swap(nums, index, i);
-            helper(nums, index + 1, result);
-            swap(nums, index, i);
+        for(int i = 0; i < arr.length; i++){
+            if(!fre[i]){
+                fre[i] = true;
+                stack.push(arr[i]);
+                find(arr, stack, ans, fre);
+                stack.pop();
+                fre[i] = false;
+            }
         }
-        return;
-    }
-    public static void swap(int[] nums, int start, int end){
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// List<List<Integer>> result = new ArrayList<>();
+//         helper(nums, 0, result);
+//         return result;
+//     }
+//     static void helper(int[] nums, int index, List<List<Integer>> result){
+//         if (index == nums.length - 1){
+//             List<Integer> temp = new ArrayList<>();
+//             for (int i = 0; i < nums.length; i++){
+//                 temp.add(nums[i]);
+//             }
+//             result.add(temp);
+//             return;
+//         }
+//         for (int i = index; i < nums.length; i++){
+//             swap(nums, index, i);
+//             helper(nums, index + 1, result);
+//             swap(nums, index, i);
+//         }
+//         return;
+//     }
+//     public static void swap(int[] nums, int start, int end){
+//         int temp = nums[start];
+//         nums[start] = nums[end];
+//         nums[end] = temp;
