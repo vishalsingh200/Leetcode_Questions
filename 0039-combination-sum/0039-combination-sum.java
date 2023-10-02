42592add910780c8dc57123d50ec71c009f9e34f
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-10-02 12:40:27 +0530

    Time: 2 ms (74.78%), Space: 43.9 MB (41.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,42 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-        LinkedList<Integer> combination = new LinkedList<>();
-        
-        this.backtrack(target, combination, 0, candidates, results);
-        return results;
+        List<List<Integer>> ans = new ArrayList<>();
+        find(0, candidates, target, ans, new Stack<>());
+        return ans;
     }
-    protected void backtrack(int remain, LinkedList<Integer> combination, int start, int [] candidates, List<List<Integer>> results){
-        if (remain == 0){
-            results.add(new ArrayList<Integer>(combination));
+    private void find(int ind, int[] arr, int target, List<List<Integer>> ans, Stack<Integer> stack){
+        if(ind == arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(stack));
+            }
             return;
         }
-        else if (remain < 0){
-            return;
+        if(arr[ind] <= target){
+            stack.add(arr[ind]);
+            find(ind, arr, target - arr[ind], ans, stack);
+            stack.pop();
         }
-        for (int i = start; i < candidates.length; i++){
-            combination.add(candidates[i]);
-            this.backtrack(remain - candidates[i], combination, i, candidates, results);
-            combination.removeLast();
-        }
+        find(ind + 1, arr, target, ans, stack);
     }
 }
+
+
+//         List<List<Integer>> results = new ArrayList<>();
+//         LinkedList<Integer> combination = new LinkedList<>();
+        
+//         this.backtrack(target, combination, 0, candidates, results);
+//         return results;
+//     }
+//     protected void backtrack(int remain, LinkedList<Integer> combination, int start, int [] candidates, List<List<Integer>> results){
+//         if (remain == 0){
+//             results.add(new ArrayList<Integer>(combination));
+//             return;
+//         }
+//         else if (remain < 0){
+//             return;
+//         }
+//         for (int i = start; i < candidates.length; i++){
+//             combination.add(candidates[i]);
+//             this.backtrack(remain - candidates[i], combination, i, candidates, results);
+//             combination.removeLast();
+ // }
