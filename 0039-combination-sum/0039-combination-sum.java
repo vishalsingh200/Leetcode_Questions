d567817e3ba3827654de494ef8cf2058878300a7
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-10-05 01:11:17 +0530

    Time: 2 ms (74.93%), Space: 43.9 MB (51.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        find(0, candidates, target, ans, new Stack<>());
+        find(candidates, 0, target, ans, new Stack<>());
         return ans;
     }
-    private void find(int ind, int[] arr, int target, List<List<Integer>> ans, Stack<Integer> stack){
+    private void find(int[] arr, int ind, int target, List<List<Integer>> ans, Stack<Integer> stack){
         if(ind == arr.length){
             if(target == 0){
                 ans.add(new ArrayList<>(stack));
@@ -12,31 +12,11 @@ class Solution {
             return;
         }
         if(arr[ind] <= target){
-            stack.add(arr[ind]);
-            find(ind, arr, target - arr[ind], ans, stack);
+            stack.push(arr[ind]);
+            find(arr, ind, target - arr[ind], ans, stack);
             stack.pop();
+            
         }
-        find(ind + 1, arr, target, ans, stack);
+        find(arr, ind + 1, target, ans, stack);
     }
 }
-
-
-//         List<List<Integer>> results = new ArrayList<>();
-//         LinkedList<Integer> combination = new LinkedList<>();
-        
-//         this.backtrack(target, combination, 0, candidates, results);
-//         return results;
-//     }
-//     protected void backtrack(int remain, LinkedList<Integer> combination, int start, int [] candidates, List<List<Integer>> results){
-//         if (remain == 0){
-//             results.add(new ArrayList<Integer>(combination));
-//             return;
-//         }
-//         else if (remain < 0){
-//             return;
-//         }
-//         for (int i = start; i < candidates.length; i++){
-//             combination.add(candidates[i]);
-//             this.backtrack(remain - candidates[i], combination, i, candidates, results);
-//             combination.removeLast();
- // }
